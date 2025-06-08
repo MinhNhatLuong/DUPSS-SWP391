c141387da8258236d6e10a5351aacab4bb65fecf
LamGia <laml33366@gmail.com>
2025-06-08 15:26:55 +0700

    update blogs(add api get 3 lastest and search blogs)

--- BE_Dupss/src/main/java/com/dupss/app/BE_Dupss/service/BlogService.java
+++ BE_Dupss/src/main/java/com/dupss/app/BE_Dupss/service/BlogService.java
@@ -1,7 +1,10 @@
 package com.dupss.app.BE_Dupss.service;
 
 import com.dupss.app.BE_Dupss.dto.request.BlogRequest;
+import com.dupss.app.BE_Dupss.dto.response.BlogHomeResponse;
 import com.dupss.app.BE_Dupss.dto.response.BlogResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -12,4 +15,6 @@ public interface BlogService {
     List<BlogResponse> getBlogsByAuthor(String authorName);
     BlogResponse getBlogById(Long id);
     List<BlogResponse> getCreatedBlogs();
+    List<BlogHomeResponse> getLatestBlogs();
+    Page<BlogHomeResponse> searchBlogs(String keyword, String tags, Pageable pageable);
 }
