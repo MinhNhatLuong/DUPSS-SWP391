99b4546f9b3708328b8b6dfabf9271e7180f4e5b
LamGia <laml33366@gmail.com>
2025-06-19 00:22:49 +0700

    added title to video course and added survey result

---
+++ BE_Dupss/src/main/java/com/dupss/app/BE_Dupss/controller/SurveyController.java
@@ -0,0 +1,30 @@
+package com.dupss.app.BE_Dupss.controller;
+
+import com.dupss.app.BE_Dupss.dto.request.SurveyResultRequest;
+import com.dupss.app.BE_Dupss.dto.response.SurveyResultResponse;
+import com.dupss.app.BE_Dupss.service.SurveyService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/survey")
+@RequiredArgsConstructor
+public class SurveyController {
+
+    private final SurveyService surveyService;
+
+    @PostMapping("/results")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<SurveyResultResponse> submitSurvey(
+            @RequestBody @Valid SurveyResultRequest request) {
+        SurveyResultResponse response = surveyService.submitSurveyResult(request);
+        return ResponseEntity.ok(response);
+    }
+}
+
---
+++ BE_Dupss/src/main/java/com/dupss/app/BE_Dupss/respository/SurveyResultRepo.java
@@ -0,0 +1,7 @@
+package com.dupss.app.BE_Dupss.respository;
+
+import com.dupss.app.BE_Dupss.entity.SurveyResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SurveyResultRepo extends JpaRepository<SurveyResult, Long> {
+}
