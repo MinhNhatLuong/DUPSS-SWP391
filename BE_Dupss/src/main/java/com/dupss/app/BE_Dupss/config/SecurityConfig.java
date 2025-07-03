26f156fbe8bc9c91430420069cba0839346ceb23
Nguyễn Thành Đạt <datnguyen.coder312@gmail.com>
2025-07-03 15:46:31 +0700

    update CORS deloy

--- BE_Dupss/src/main/java/com/dupss/app/BE_Dupss/config/SecurityConfig.java
+++ BE_Dupss/src/main/java/com/dupss/app/BE_Dupss/config/SecurityConfig.java
@@ -103,7 +103,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "https://dupss.vercel.app/"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "https://dupss.vercel.app/" , "http://34.87.106.55:3000" , "http://34.87.106.55:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
