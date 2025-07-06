a54f0a0d68b1eb74f38cd2ca5be23b24732f0e82
Nguyễn Thành Đạt <datnguyen.coder312@gmail.com>
2025-07-06 17:20:36 +0700

    update deloy vite Dockerfile-dockercompose

--- frontend-dupss/vite.config.js
+++ frontend-dupss/vite.config.js
@@ -14,3 +14,22 @@ export default defineConfig({
     }
   }
 })
+// Deloy
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true, // Cho phép nhận kết nối từ bên ngoài 
+//     allowedHosts: ['dupssapp.id.vn'], // 
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8080',
+//         changeOrigin: true,
+//         secure: false
+//       }
+//     }
+//   }
+// })
