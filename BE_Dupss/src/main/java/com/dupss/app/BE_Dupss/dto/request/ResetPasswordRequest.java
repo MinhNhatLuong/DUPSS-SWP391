30b3b0fc73ff34773d7561cc8547f8387ed938ad
LamGia <laml33366@gmail.com>
2025-07-22 15:36:59 +0700

    add validation for password reset

--- BE_Dupss/src/main/java/com/dupss/app/BE_Dupss/dto/request/ResetPasswordRequest.java
+++ BE_Dupss/src/main/java/com/dupss/app/BE_Dupss/dto/request/ResetPasswordRequest.java
@@ -25,5 +25,7 @@ public class ResetPasswordRequest {
     
     @NotBlank(message = "Mật khẩu mới không được để trống")
     @Size(min = 6, message = "Mật khẩu phải có ít nhất 6 ký tự")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$",
+            message = "Mật khẩu phải chứa ít nhất một chữ cái viết hoa, một chữ cái viết thường, một số và một ký tự đặc biệt")
     private String newPassword;
 } 
